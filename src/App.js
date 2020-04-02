40f78f49f94da1d47edfc029fe574fbb6c05af9f
akiakmapro@gmail.com <akiakmapro@gmail.com>
2020-04-03 00:49:39 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import Posts from './componenets/Posts';
+import Pagination from './componenets/Pagination';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] =useState([]);
+  const [loading, setLoading] = useState([false]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage ] = useState(10);
+
+  useEffect(() => {
+     const fetchPosts = async () => {
+       setLoading(true);
+       const res = await axios.get('http://jsonplaceholder.typicode.com/posts');
+       setPosts(res.data);
+       setLoading(false);
+     }
+
+     fetchPosts();
+  }, [])
+
+  // Get current posts
+  const indexOfLastPosts = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPosts - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPosts)
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  console.log(posts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My blog</h1>
+      <Posts posts={currentPosts} loading={loading}/>
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
